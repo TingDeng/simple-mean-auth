d1db1fa98dd743b999c869e48e3abcbf01e2416c
Ting <tiadeng@gmail.com>
2016-03-31 19:36:02 -0400

    routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -28,6 +28,27 @@ router.post('/register', function(req, res) {
 router.post('/login', function(req, res, next) {
   //login route for logging in existing users
   // What do you notice about this function?
+  //next is kick them to next round...
+  passport.authenticate('local',function(err, user, info){
+  if(err){
+     return next(err);
+  }
+  if(!user){
+    return res.status(401).json({
+      err:info
+    });//unauthoried access is 401
+  }//sign in without account
+  req.logIn(user,function(err){
+    if(err){
+      return res.status(500).json({
+        err:'Could not log in user'
+      })
+    }
+    res.status(200).json({
+      status:'Login successful'
+    });
+  })//during login, maybe some connection error
+})(req,res,next);
 });
 
 router.get('/logout', function(req, res) {
@@ -37,10 +58,19 @@ router.get('/logout', function(req, res) {
      status:'Bye'
    });
 });
-
+//../means come out and get back in
 router.get('/status', function(req, res) {
   //Why do we need a status route?
-});///user/login or logout or..
+  if(!req.isAuthenticated()){
+    return res.status(200).json({
+      status: false,
+    });
+  }
+  res.status(200).json({
+    status:true
+  });
+});///user/login or logout or..//login is post, get:status,logout is get;
 
+//$scope is when code interact with page, 
 
 module.exports = router;
